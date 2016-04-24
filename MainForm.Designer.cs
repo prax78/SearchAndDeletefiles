1196584b64abf19c735dd73ca0194718ffa5c22b
prax78 <prax78@gmail.com>
2016-04-24 18:24:44 +0530

    Added files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: narayanan
+ * Date: 4/23/2016
+ * Time: 2:01 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace listfiles
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// folderBrowserDialog1
+			// 
+			this.folderBrowserDialog1.ShowNewFolderButton = false;
+			// 
+			// button1
+			// 
+			this.button1.FlatAppearance.BorderSize = 5;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(195, 46);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Open Folder to Search for Files";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(80, 306);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(368, 23);
+			this.label1.TabIndex = 2;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.HorizontalScrollbar = true;
+			this.listBox1.Location = new System.Drawing.Point(80, 130);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(498, 173);
+			this.listBox1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(584, 130);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(88, 102);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Save List Contents to File";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.DefaultExt = "\"txt\"";
+			this.saveFileDialog1.Filter = "\"Text files (*.txt)|*.txt|All files (*.*)|*.*\"";
+			// 
+			// button3
+			// 
+			this.button3.FlatAppearance.BorderSize = 5;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.button3.Location = new System.Drawing.Point(530, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(133, 46);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Clear List";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(205, 93);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(146, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(39, 93);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(160, 34);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Find Files based on pattern (*.log etc)";
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.Color.Red;
+			this.button4.Location = new System.Drawing.Point(584, 238);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(88, 65);
+			this.button4.TabIndex = 8;
+			this.button4.Text = "Delete List Files from HDD";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(554, 94);
+			this.textBox2.MaxLength = 4;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(57, 20);
+			this.textBox2.TabIndex = 9;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(429, 90);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(110, 37);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "Find Files Modified since (older) in days";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(617, 91);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(64, 23);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Days";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.radioButton1.Location = new System.Drawing.Point(12, 90);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(21, 24);
+			this.radioButton1.TabIndex = 12;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.BackColor = System.Drawing.SystemColors.Control;
+			this.radioButton2.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.radioButton2.Location = new System.Drawing.Point(404, 94);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(28, 22);
+			this.radioButton2.TabIndex = 13;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.UseVisualStyleBackColor = false;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2CheckedChanged);
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.Blue;
+			this.label5.Location = new System.Drawing.Point(12, 67);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(263, 23);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "Filter Type 1:Choose by Selecting Checkbox";
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.Color.Blue;
+			this.label6.Location = new System.Drawing.Point(404, 68);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(259, 23);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "Filter Type 2:Choose by Selecting CheckBox";
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(483, 342);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(189, 23);
+			this.label8.TabIndex = 17;
+			this.label8.Text = "Use at your own risk";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.Location = new System.Drawing.Point(483, 319);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(180, 23);
+			this.linkLabel1.TabIndex = 18;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "pr_prakash78@outlook.com";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 358);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Search and Delete Files v0.1 Beta";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		}
+	}
+
---
+++ MainForm.cs
@@ -0,0 +1,265 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: narayanan
+ * Date: 4/23/2016
+ * Time: 2:01 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace listfiles
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{public string Filter;
+		public double days=0;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+			if(listBox1.Items.Count>0)
+			{
+				listBox1.Items.Clear();
+				
+			}
+			
+			if(radioButton1.Checked)
+			   {
+				
+					if(textBox1.Text!="")
+						{
+								button2.Enabled=true;
+								button4.Enabled=true;
+								string selection=folderBrowserDialog1.ShowDialog().ToString();
+								if(selection!=DialogResult.Cancel.ToString())
+								{
+								string path=folderBrowserDialog1.SelectedPath;
+								Filter=textBox1.Text.ToString();	
+							
+			 												
+			 					 getsubfiles(path);	 	
+			 				
+							    label1.Text="Total Object Found:"+listBox1.Items.Count;
+								}
+						}
+					else
+						{
+							MessageBox.Show("file filter option should not be blank",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+						}
+				}
+			
+			
+			if(radioButton2.Checked)
+			{
+				
+				if(textBox2.Text!="")
+					{
+						
+						
+								button2.Enabled=true;
+								button4.Enabled=true;
+								string selection1=folderBrowserDialog1.ShowDialog().ToString();
+								if(selection1!=DialogResult.Cancel.ToString())
+								{
+								string path1=folderBrowserDialog1.SelectedPath;
+								days=Convert.ToDouble( textBox2.Text);
+								if((days <0) || (days > 6000))
+								{
+									MessageBox.Show("Enter Value between 0 and 6000",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+								}
+								   
+								days=(-1*days);
+			 			getsubfiles1(path1);
+			 			 label1.Text="Total Files Found:"+listBox1.Items.Count;
+								}
+					}
+				else
+						{
+							MessageBox.Show("file filter option should not be blank",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+						}
+				}
+			
+		}
+		
+		void getsubfiles(string p)
+		{
+			try
+			{
+			foreach(string d in System.IO.Directory.GetDirectories(p))
+			{
+				getsubfiles(d);
+			}
+				
+			foreach(string f in System.IO.Directory.GetFiles(p,Filter))
+			{
+				listBox1.Items.Add(f);
+			}
+			}
+			catch(Exception e)
+			{
+				listBox1.Items.Add(e.Message);
+			}
+			
+		}
+		void Button2Click(object sender, EventArgs e) 
+		{
+			//saveFileDialog1.ShowDialog();
+			
+			if(listBox1.Items.Count!=0)
+			{
+			string diagresult=saveFileDialog1.ShowDialog().ToString();
+			string filename=saveFileDialog1.FileName;
+			if(diagresult!=DialogResult.Cancel.ToString())
+			{
+			System.IO.StreamWriter wrf=new System.IO.StreamWriter(filename);
+			
+			foreach(object item in listBox1.Items)
+			{
+				wrf.WriteLine(item);
+			}
+		
+			wrf.Close();
+			
+			wrf.Dispose();
+			MessageBox.Show("File Saved "+filename,this.Text,MessageBoxButtons.OK,MessageBoxIcon.Information);
+			}
+			else
+			{
+				MessageBox.Show("you have not selected files",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+			}
+			}
+			else{
+				MessageBox.Show("List is empty",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+			}
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			label1.Text="";
+			button2.Enabled=false;
+			button4.Enabled=false;
+			textBox1.Text="";
+			textBox2.Text="";
+			radioButton1.Enabled=true;
+			radioButton2.Enabled=true;
+			textBox1.Enabled=true;
+			textBox2.Enabled=true;
+		
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	if(listBox1.Items.Count==0)
+			{
+				button2.Enabled=false;
+				button4.Enabled=false;
+			}
+	
+
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			System.Text.StringBuilder sb=new System.Text.StringBuilder();
+			if(listBox1.Items.Count!=0)
+			{
+			string msgres=MessageBox.Show("You are about to delete files listed here,Select Yes to delete files or No to close application",this.Text,MessageBoxButtons.YesNo,MessageBoxIcon.Error,MessageBoxDefaultButton.Button2).ToString();
+			if(msgres=="Yes")
+			{
+				foreach(object item in listBox1.Items)
+				{try{
+					System.IO.File.Delete(item.ToString());
+					}
+					catch(Exception ex){
+						sb.Append(ex.Message.ToString());
+						sb.Append(Environment.NewLine);
+					}
+				}
+				if(sb.Length!=0)
+				{
+				MessageBox.Show(sb.ToString(),this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+				}
+				MessageBox.Show("Listed files are Deleted",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Information);
+			}
+			else
+			{
+				this.Close();
+				
+			}
+			}
+			else
+			{
+				MessageBox.Show("List is empty",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+			}
+			
+		}
+		
+		void getsubfiles1(string m)
+		{
+			try
+			{
+			foreach(string d in System.IO.Directory.GetDirectories(m))
+			{
+				getsubfiles1(d);
+			}
+			
+			foreach(string f in System.IO.Directory.GetFiles(m))
+			{
+				if(System.IO.File.GetLastWriteTime(f)< System.DateTime.Now.AddDays(days))
+				{
+				listBox1.Items.Add(f);
+				}
+			}
+			
+			}
+			catch(Exception e)
+			{
+				listBox1.Items.Add(e.Message);
+			}
+			
+		}
+		void RadioButton2CheckedChanged(object sender, EventArgs e)
+		{
+			
+				textBox1.Enabled=false;
+				textBox2.Enabled=true;
+				
+		}
+		void RadioButton1CheckedChanged(object sender, EventArgs e)
+		{
+	
+				textBox2.Enabled=false;
+				textBox1.Enabled=true;
+				
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			
+			
+			
+			
+			
+		}
+		void Timer2Tick(object sender, EventArgs e)
+		{
+	
+		}
+		
+		
+	}
+}
+
